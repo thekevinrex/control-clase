d23db6ba86e254008acdd2fedc4284102a9e2bcd
kleyris02 <159505943+kleyris02@users.noreply.github.com>
2024-02-08 23:17:56 -0500

    dgfsdfsd (#1)
    * sdfsd
    
    * actualice lenguaje departamento

--- resources/views/departament/show.blade.php
+++ resources/views/departament/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Departaments') }}
+            {{ __('Departamentos') }}
         </h2>
 
         <Link href="{{ route('departament.create') }}" slideover>Add departament</Link>
